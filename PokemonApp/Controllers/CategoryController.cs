f4ae0ec4e5528c9b6e6d8ebf0807c610273d4f63
Timi Precious <pt.timonegmail.com>
2024-01-06 12:41:20 +0100

    setup controllers and models

---
+++ PokemonApp/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonApp.Interfaces;
+using PokemonApp.Models;
+
+namespace PokemonApp.Controllers
+{
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoryController(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+
+
+        }
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Category>))]
+
+
+        public IActionResult GetCategories()
+        {
+            var categories = _mapper.Map<List>
+        }
+    }
+}
--- PokemonApp/Repository/CategoryRepository.cs
+++ PokemonApp/Repository/CategoryRepository.cs
@@ -1,8 +1,40 @@
-﻿namespace PokemonApp.Repository
+﻿using PokemonApp.Data;
+using PokemonApp.Interfaces;
+using PokemonApp.Models;
+
+namespace PokemonApp.Repository
 {
-    public class CategoryRepository
+    public class CategoryRepository : ICategoryRepository
     {
+        private readonly DataContext _context;
 
+        public CategoryRepository(DataContext context)
+        {
 
+            _context = context;
+
+        }
+        public bool CategoryExists(int id)
+        {
+            return _context.Categories.Any(p => p.Id == id);
+        }
+
+        public ICollection<Category> GetCategories()
+        {
+            return _context.Categories.OrderBy(c => c.Id).ToList();
+        }
+
+        public Category GetCategory(int id)
+        {
+            return _context.Categories.Where(c => c.Id == id).FirstOrDefault();
+        }
+
+        public ICollection<Pokemon> GetPokeMonByCategory(int id)
+        {
+            return _context.PokemonCategories
+                  .Where(pc => pc.CategoryId == id)
+                  .Select(c => c.Pokemon)
+                  .ToList();
+        }
     }
 }
